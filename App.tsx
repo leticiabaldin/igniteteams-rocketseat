1b244a18dcbdddfb171730f99a81de289cb4f175
Leticia Baldin <leticiagalvani2003@gmail.com>
2025-02-04 23:14:02 -0300

    add theme styles/fonts

--- App.tsx
+++ App.tsx
@@ -1,7 +1,21 @@
 import { Groups } from '@screens/Groups'; //path mapping
+import theme from '@theme/index';
+import { ThemeProvider } from 'styled-components';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { Loading } from '@components/Loading';
+import { StatusBar } from 'react-native';
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({    //fontes carregadas de forma assincrona
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
   return (
-    <Groups />
+    <ThemeProvider theme={theme}>
+      <StatusBar barStyle={'light-content'}/>
+
+     {fontsLoaded? <Groups /> : <Loading /> } 
+    </ThemeProvider>
   );
 }
---
+++ src/components/Loading/styles.ts
@@ -0,0 +1,17 @@
+import { ActivityIndicator, View } from "react-native";
+import styled from "styled-components";
+
+export const Container = styled(View)`
+
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+
+    background: ${({ theme }) => theme.COLORS.GRAY_600};
+`;
+
+export const LoadIndicator = styled(ActivityIndicator).attrs(( { theme }) => ({
+    color: theme.COLORS.GREEN_700
+}))`
+
+`;
